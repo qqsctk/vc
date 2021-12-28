0b098f00ba2761cf6e1237fb65027d48c0aa52ee
Alex <aleksandrkondratev91@gmail.com>
2021-12-28 14:36:58 +0300

    changes for express promo_create

--- cypress/integration/vc.ru/cabinet/express_promo_create.js
+++ cypress/integration/vc.ru/cabinet/express_promo_create.js
@@ -1,31 +1,47 @@
-describe('express promo create', () => {
+describe('express promo creating', () => {
     before(() => {
-        cy
+      cy
           .login('a.kondratev@cmtt.ru', 'q')
-          .wait(1000);
+          .visit('/u/927443-test-com/')
+          .get('.content-title').invoke('text').as('title')
+
       })
       beforeEach(() => {
-        Cypress.Cookies.preserveOnce('osnova-aid', 'osnova-remember');
+        Cypress.Cookies.preserveOnce('osnova-aid', 'osnova-remember')
       })
 
+
+
     it('choose post and open constructor', () => {
-    cy.visit('/u/927443-test-com')
-    cy.get('.etc_control:first').click()
-    cy.contains('Продвинуть').click()
-    cy.get('v-popup-window__content')
+      cy.get('.etc_control:first').click()
+      cy.contains('Продвинуть').click()
+      cy.get('.v-popup-container.constructor-popup-container .v-popup-window__content')
        .should('be.exist')
        
     })
-    it('check booster form title in constractor form', () => {
-    cy.get('.booster-constructor__title')
-      .should('have.css', 'font-size', '32px')
-      .should('have.text', '\n Продвинуть статью\n ')
+    it('check title in constractor form', () => {
+      cy.get('.booster-constructor__title')
+        .should('have.css', 'font-size', '32px')
+        .should('have.text', '\n    Продвинуть статью\n  ')
     })
 
-    it('check preview', () => {
+    it('check preview in contractor form', () => {
     cy.get('.bp-unit__link')
       .should('have.attr', 'href')
     //cy.get('.bp-unit-entry__subsite-avatar').should('be.visible')
     cy.get('.bp-unit-entry__subsite-name').should('have.text', '\n      Test Com\n    ')
+    cy.get('.bp-unit-entry__title').invoke('text').should('equal', '@title')
     })
+
+    it('check hits form, submit button and redirect to cab', () => {
+      cy.get('.t-link-inline').should('have.attr', 'href')
+      cy.get('.v-range__handle').should('be.visible')
+        .contains('Продолжить').should('be.visible').click()
+      cy.url().should('eq', Cypress.config().baseUrl + '/cabinet/services')
+      })
+    it('check express promo added in cab', () => {
+      cy.get('cs-list-item__title:first').should('equal', '@title')
+        .get('cs-list-item__status').should('have.text', 'Не запущено')
+        //.get('cs-list-item__info').should()
+    })   
 })    
